8634a616cd1c713df900a673bf8260bad11d9c4e
unknown <359206421@qq.com>
2023-05-17 17:29:16 +0800

    Update app.config.ts

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,9 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
+  pages: ['pages/Index/Index'],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    navigationBarTextStyle: 'black',
+  },
+});
