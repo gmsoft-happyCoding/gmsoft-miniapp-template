b3dda2738135c53a179ea439309352e85402fee4
unknown <359206421@qq.com>
2023-09-01 18:00:00 +0800

    Update build.ts

--- config/utils/build.ts
+++ config/utils/build.ts
@@ -1,18 +1,18 @@
-import { spawnSync } from 'child_process';
+import { execSync } from 'child_process';
 import { AppType } from '../enums/AppType.enum';
 import { BuildType } from '../enums/BuildType.enum';
 
 const build = (miniType: AppType, buildType: BuildType, mode?: boolean) => {
+  console.log(`打包模式:${buildType}`);
+
+  console.log(process.env.MINI_APP_SUBPACKAGE_CONFIG);
+
   // 编译主包
-  spawnSync(
-    'taro',
-    [
-      `build --type ${miniType} ${
-        mode ? (buildType === BuildType.SUB_PACKAGE ? '--blended' : '') : '--watch'
-      }`,
-    ],
+  execSync(
+    `taro build --type ${miniType} ${
+      mode ? (buildType === BuildType.SUB_PACKAGE ? '--blended' : '') : '--watch'
+    }`,
     {
-      shell: true,
       stdio: 'inherit',
       cwd: process.cwd(),
     }
