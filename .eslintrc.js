9fb43713c1a8ca9f96639479da3d14cf50886852
unknown <359206421@qq.com>
2023-11-23 16:05:02 +0800

    feat：修改eslint配置文件类型
    更新eslint的提示操作。

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['taro/react'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'jsx-quotes': ['error', 'prefer-double'],
+    'import/no-commonjs': 'off',
+    'import/no-named-default': 'off',
+  },
+};
